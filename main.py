9500ab7e14b808cab3aac0e625604a57d771debb
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2023-01-20 16:09:02 +0700

    Error Fixed

--- main.py
+++ main.py
@@ -23,78 +23,60 @@ print("===========================================")
 print('PERINGATAN : TIDAK UNTUK DI PERJUAL-BELIKAN | TOLONG HARGAI PEMBUAT')
 print("===========================================\n")
 
-# Menu 1
-def menu1():
-    print("Menu 1")
-    print("Installing...")
-    os.system("apt upgrade && update")
-    os.system("apt install openssl")
-    print("Installed! Selesai Lanjut ke Nomer 2")
-
 time.sleep(1)
 
-# Menu 2
-def menu2():
-    channel_id = input("Masukkan ID channel: ")
-    waktu1 = int(input("Set Waktu Hapus Pesan: "))
-    waktu2 = int(input("Set Waktu Kirim Pesan: "))
-    
-    os.system('cls' if os.name == 'nt' else 'clear')
+channel_id = input("Masukkan ID channel: ")
+waktu1 = int(input("Set Waktu Hapus Pesan: "))
+waktu2 = int(input("Set Waktu Kirim Pesan: "))
 
-    with open("pesan.txt", "r") as f:
-        words = f.readlines()
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
 
-    with open("token.txt", "r") as f:
-        authorization = f.readline().strip()
+os.system('cls' if os.name == 'nt' else 'clear')
 
-    while True:
-            channel_id = channel_id.strip()
+with open("pesan.txt", "r") as f:
+    words = f.readlines()
 
-            payload = {
-                'content': random.choice(words).strip()
-            }
+with open("token.txt", "r") as f:
+    authorization = f.readline().strip()
 
-            headers = {
-                'Authorization': authorization
-            }
-
-            r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
-            print(Fore.WHITE + "Sent message: ")
-            print(Fore.YELLOW + payload['content'])
-
-            response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
-
-            if response.status_code == 200:
-                messages = response.json()
-                if len(messages) == 0:
-                    is_running = False
-                    break
-                else:
-                    time.sleep(waktu1)
-
-                    message_id = messages[0]['id']
-                    response = requests.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}', headers=headers)
-                    if response.status_code == 204:
-                        print(Fore.GREEN + f'Pesan dengan ID {message_id} berhasil dihapus')
-                    else:
-                     print(Fore.RED + f'Gagal menghapus pesan dengan ID {message_id}: {response.status_code}')
-            else:
-                print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
-
-            time.sleep(waktu2)
-
-# Pemanggilan menu
 while True:
-    print("Menu Nya bang Siapkan Kopi Rokonya:\n")
-    print("1. Instal Openssl")
-    print("2. Jalankan Program")
-    print("3. Keluar\n")
-    choice = int(input("Masukkan pilihan: \n"))
-    if choice == 1:
-        menu1()
-    elif choice == 2:
-        menu2()
-    elif choice == 3:
-        break
-    else:
-        print("Pilihan tidak valid, silakan coba lagi.")
+        channel_id = channel_id.strip()
+
+        payload = {
+            'content': random.choice(words).strip()
+        }
+
+        headers = {
+            'Authorization': authorization
+        }
+
+        r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+        print(Fore.WHITE + "Sent message: ")
+        print(Fore.YELLOW + payload['content'])
+
+        response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+
+        if response.status_code == 200:
+            messages = response.json()
+            if len(messages) == 0:
+                is_running = False
+                break
+            else:
+                time.sleep(waktu1)
+
+                message_id = messages[0]['id']
+                response = requests.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}', headers=headers)
+                if response.status_code == 204:
+                    print(Fore.GREEN + f'Pesan dengan ID {message_id} berhasil dihapus')
+                else:
+                    print(Fore.RED + f'Gagal menghapus pesan dengan ID {message_id}: {response.status_code}')
+        else:
+            print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
+
+        time.sleep(waktu2)
