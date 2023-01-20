ce93532e4482b84648ec91706498444e8c1a23ee
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2023-01-20 11:18:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+import random
+import time
+import json
+from colorama import Fore
+from asciimatics.renderers import FigletText
+
+author = "Bg.Pateng"
+print(FigletText(author, font='small'))
+print("=========================================")
+author = "Bg.Pateng"
+print("Author: " + author)
+script = "Push Rank Discord"
+print("Script: " + script)
+telegram = "@bangpateng_group"
+print("Telegram: " + telegram)
+youtube = "Bang Pateng"
+print("Youtube: " + youtube)
+
+print("===========================================")
+print('PERINGATAN : TIDAK UNTUK DI PERJUAL-BELIKAN')
+print("===========================================")
+
+time.sleep(1)
+
+channel_id = input("Masukkan ID channel Discord Target: ")
+waktu1 = int(input("Set Waktu Hapus Pesan: "))
+waktu2 = int(input("Set Waktu Kirim Pesan: "))
+
+with open("pesan.txt", "r") as f:
+    words = f.readlines()
+
+with open("token.txt", "r") as f:
+    authorization = f.readline().strip()
+
+while True:
+        channel_id = channel_id.strip()
+
+        payload = {
+            'content': random.choice(words).strip()
+        }
+
+        headers = {
+            'Authorization': authorization
+        }
+
+        r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+        print(Fore.WHITE + "Sent message: ")
+        print(Fore.YELLOW + payload['content'])
+
+        response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+
+        if response.status_code == 200:
+            messages = response.json()
+            if len(messages) == 0:
+                is_running = False
+                break
+            else:
+                time.sleep(waktu1)
+
+                message_id = messages[0]['id']
+                response = requests.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}', headers=headers)
+                if response.status_code == 204:
+                    print(Fore.GREEN + f'Pesan dengan ID {message_id} berhasil dihapus')
+                else:
+                    print(Fore.RED + f'Gagal menghapus pesan dengan ID {message_id}: {response.status_code}')
+        else:
+            print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
+
+        time.sleep(waktu2)
